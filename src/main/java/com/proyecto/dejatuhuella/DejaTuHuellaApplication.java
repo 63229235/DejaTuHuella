2281631550f814300a3b5a0e69c7465e2586dadb
Piero Revolledo <112639712+63229235@users.noreply.github.com>
2025-05-15 19:07:27 -0500

    Initial commit

---
+++ src/main/java/com/proyecto/dejatuhuella/DejaTuHuellaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.dejatuhuella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DejaTuHuellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DejaTuHuellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/dejatuhuella/DejaTuHuellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.dejatuhuella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DejaTuHuellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
